6638f79a29ad14c68c9338474ea01439e1354dc6
Phuong Ngan <phankhanhdn12@gmail.com>
2026-06-24 19:57:31 +0700

    feat: add sequelize models and database migrations

---
+++ migrations/20260624114509-create-borrow-record.js
@@ -0,0 +1,63 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('BorrowRecords', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+
+      bookId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Books',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+
+      status: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: 'BORROWED',
+      },
+
+      borrowDate: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+
+      returnDate: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('BorrowRecords');
+  }
+};
